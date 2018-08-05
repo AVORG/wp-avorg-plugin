76c62fe30defa1b5bd6a1eaa62b2c10624fd4964
Nathan Arthur <nathan@nathanarthur.com>
2018-08-05 10:50:30 -0400

    Extract success notice into its own mocule

--- wp-avorg.php
+++ wp-avorg.php
@@ -26,13 +26,13 @@ $router = $factory->getRouter();
 \register_deactivation_hook(__FILE__, "plugin_deactivate");
 
 \add_action("admin_menu", array($adminPanel, "register"));
-\add_action('init', array($plugin, "init"));
+\add_action("init", array($plugin, "init"));
 \add_action("add_meta_boxes", array($contentBits, "addIdentifierMetaBox"));
-\add_action('save_post', array($contentBits, "saveIdentifierMetaBox"));
+\add_action("save_post", array($contentBits, "saveIdentifierMetaBox"));
 \add_action("wp_enqueue_scripts", array($plugin, "enqueueScripts"));
 
-\add_filter('the_content', array($mediaPage, "addMediaPageUI"));
-\add_filter('locale', array($router, "setLocale"));
+\add_filter("the_content", array($mediaPage, "addMediaPageUI"));
+\add_filter("locale", array($router, "setLocale"));
 
 function plugin_deactivate()
 {
