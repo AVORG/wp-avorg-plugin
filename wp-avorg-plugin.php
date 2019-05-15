529c19d9000fba7f29edc2d927305d6eed43da92
Nathan Arthur <nathan@nathanarthur.com>
2019-05-15 17:31:32 -0400

    Make namespaces explicit in main plugin file

--- wp-avorg-plugin.php
+++ wp-avorg-plugin.php
@@ -11,6 +11,8 @@ Domain Path: /languages
 
 namespace Avorg;
 
+use natlib\Factory;
+
 if (!\defined('ABSPATH')) exit;
 
 define( "AVORG_BASE_PATH", dirname(__FILE__) );
@@ -21,6 +23,6 @@ include_once(AVORG_BASE_PATH . "/vendor/autoload.php");
 
 $factory = new Factory();
 
-$factory->secure("AdminPanel")->registerCallbacks();
+$factory->secure("Avorg\\AdminPanel")->registerCallbacks();
 
-\register_activation_hook(__FILE__, [$factory->secure("Plugin"), "activate"]);
+\register_activation_hook(__FILE__, [$factory->secure("Avorg\\Plugin"), "activate"]);
