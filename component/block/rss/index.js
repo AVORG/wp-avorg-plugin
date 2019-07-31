e6307c7c284f02e0f8ed64053e5779f5b548bcce
Nathan Arthur <nathan@nathanarthur.com>
2019-07-31 16:00:45 -0400

    Build out rss link block

--- component/block/rss/index.js
+++ component/block/rss/index.js
@@ -2,6 +2,10 @@ var blocks = wp.blocks;
 var element = wp.element;
 var el = element.createElement;
 var blockStyle = {};
+var createLink = function (url) {
+    var Icon = wp.components.Icon;
+    return el('p', {}, el('a', { href: url, target: "_blank" }, el(Icon, { icon: 'rss' })));
+};
 blocks.registerBlockType('avorg/block-rss', {
     title: 'RSS Link',
     icon: 'rss',
@@ -17,14 +21,19 @@ blocks.registerBlockType('avorg/block-rss', {
     edit: function (props) {
         var url = props.attributes.url, isSelected = props.isSelected, setAttributes = props.setAttributes;
         var URLInput = wp.editor.URLInput;
-        return el('p', { style: blockStyle, className: props.className }, isSelected ? el(URLInput, {
+        var urlInput = el(URLInput, {
             placeholder: 'RSS Url',
             value: url,
             onChange: function (url) { return setAttributes({ url: url }); }
-        }) : 'RSS');
+        });
+        var form = el('form', {
+            onSubmit: function (event) { return event.preventDefault(); }
+        }, urlInput);
+        return el('div', { style: blockStyle, className: props.className }, isSelected ? form : createLink(url));
     },
     save: function (props) {
-        return el('p', { style: blockStyle }, 'RSS');
+        var url = props.attributes.url;
+        return createLink(url);
     },
 });
 //# sourceMappingURL=index.js.map
--- component/block/rss/index.ts
+++ component/block/rss/index.ts
@@ -10,6 +10,14 @@ const blockStyle = {
     // padding: '20px',
 };
 
+const createLink = (url: string) => {
+    const {
+        Icon
+    } = wp.components;
+
+    return el('p', {}, el('a', {href: url, target: "_blank"}, el(Icon, {icon: 'rss'})));
+};
+
 blocks.registerBlockType('avorg/block-rss', {
     title: 'RSS Link',
     icon: 'rss',
@@ -29,21 +37,25 @@ blocks.registerBlockType('avorg/block-rss', {
             URLInput
         } = wp.editor;
 
+        const urlInput = el(URLInput, {
+            placeholder: 'RSS Url',
+            value: url,
+            onChange: (url: string) => setAttributes({url})
+        });
+
+        const form = el('form', {
+            onSubmit: (event: Event) => event.preventDefault()
+        }, urlInput);
+
         return el(
-            'p',
+            'div',
             {style: blockStyle, className: props.className},
-            isSelected ? el(URLInput, {
-                placeholder: 'RSS Url',
-                value: url,
-                onChange: (url: string) => setAttributes({ url })
-            }) : 'RSS'
+            isSelected ? form : createLink(url)
         );
     },
     save: function (props: any) {
-        return el(
-            'p',
-            {style: blockStyle},
-            'RSS'
-        );
+        const { attributes: { url } } = props;
+
+        return createLink(url);
     },
 });
