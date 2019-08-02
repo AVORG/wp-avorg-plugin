9d1a205da2001a9d01e96fb7b5bb0edba4403cbb
Nathan Arthur <nathan@nathanarthur.com>
2019-08-02 10:45:02 -0400

    Start building Placeholder component

--- component/block/relatedSermons/index.js
+++ component/block/relatedSermons/index.js
@@ -1,6 +1,6 @@
 System.register(["./shared.js"], function (exports_1, context_1) {
     "use strict";
-    var shared_js_1, blocks, element, el, blockStyle;
+    var shared_js_1, AvorgRelatedSermons;
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [
@@ -9,22 +9,24 @@ System.register(["./shared.js"], function (exports_1, context_1) {
             }
         ],
         execute: function () {
-            blocks = wp.blocks;
-            element = wp.element;
-            el = element.createElement;
-            blockStyle = {};
-            blocks.registerBlockType('avorg/block-relatedsermons', {
-                title: 'Related Sermons',
-                icon: 'excerpt-view',
-                category: 'widgets',
-                edit: function (props) {
-                    shared_js_1.loadRecordings(props.className);
-                    return el('p', { style: blockStyle, className: props.className }, 'Loading...');
-                },
-                save: function (props) {
-                    return el('p', { style: blockStyle }, 'Loading...');
-                },
-            });
+            (function (AvorgRelatedSermons) {
+                var blocks = wp.blocks;
+                var element = wp.element;
+                var el = element.createElement;
+                var blockStyle = {};
+                blocks.registerBlockType('avorg/block-relatedsermons', {
+                    title: 'Related Sermons',
+                    icon: 'excerpt-view',
+                    category: 'widgets',
+                    edit: function (props) {
+                        shared_js_1.loadRecordings(props.className);
+                        return el('p', { style: blockStyle, className: props.className }, 'Loading...');
+                    },
+                    save: function (props) {
+                        return el('p', { style: blockStyle }, 'Loading...');
+                    },
+                });
+            })(AvorgRelatedSermons || (AvorgRelatedSermons = {}));
         }
     };
 });
--- component/block/relatedSermons/index.ts
+++ component/block/relatedSermons/index.ts
@@ -1,35 +1,37 @@
 import { loadRecordings } from "./shared.js";
 
-declare const wp: any;
+namespace AvorgRelatedSermons {
+    declare const wp: any;
 
-const blocks = wp.blocks;
-const element = wp.element;
-const el = element.createElement;
+    const blocks = wp.blocks;
+    const element = wp.element;
+    const el = element.createElement;
 
-const blockStyle = {
-    // backgroundColor: '#900',
-    // color: '#fff',
-    // padding: '20px',
-};
+    const blockStyle = {
+        // backgroundColor: '#900',
+        // color: '#fff',
+        // padding: '20px',
+    };
 
-blocks.registerBlockType('avorg/block-relatedsermons', {
-    title: 'Related Sermons',
-    icon: 'excerpt-view',
-    category: 'widgets',
-    edit: function (props: any) {
-        loadRecordings(props.className);
+    blocks.registerBlockType('avorg/block-relatedsermons', {
+        title: 'Related Sermons',
+        icon: 'excerpt-view',
+        category: 'widgets',
+        edit: function (props: any) {
+            loadRecordings(props.className);
 
-        return el(
-            'p',
-            {style: blockStyle, className: props.className},
-            'Loading...'
-        );
-    },
-    save: function (props: any) {
-        return el(
-            'p',
-            {style: blockStyle},
-            'Loading...'
-        );
-    },
-});
+            return el(
+                'p',
+                {style: blockStyle, className: props.className},
+                'Loading...'
+            );
+        },
+        save: function (props: any) {
+            return el(
+                'p',
+                {style: blockStyle},
+                'Loading...'
+            );
+        },
+    });
+}
